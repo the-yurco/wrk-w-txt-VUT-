5dd56acd56822846de5d5a109a37d4f2958c1dce
the-yurco <jurajbosy404@gmail.com>
2025-10-13 20:19:22 +0200

    init

---
+++ keyfilter.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+
+
+
+int main() {
+  
+  char chr;
+  printf("Enter a string: ");  
+  scanf("%c", &chr);
+  
+  return 0;
+}
