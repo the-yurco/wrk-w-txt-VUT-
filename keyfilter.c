b16854b8779cc51e76ec043a23a5d7398c20bb6e
the-yurco <jurajbosy404@gmail.com>
2025-10-15 01:53:46 +0200

    Replacing \n for \0 + added temp_buff (safe)
    - temp_buff = Temporary buffer for reading, so that we do not overwrite the structure in case of an error in buffName
    - strcspn = changing certain char
    - strcpy = copying into official buff

--- keyfilter.c
+++ keyfilter.c
@@ -3,15 +3,35 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define ROW_LENGHT 101
+#define NAME_LENGHT 101
+#define ADDRESSES_ROWS 42
+
+struct Address{
+  char buffName[NAME_LENGHT];
+};
 
 int main() {
     
   FILE* pFile = stdin;
-  char buffAddresses[ROW_LENGHT];
+  
+  struct Address addresses[ADDRESSES_ROWS];
+  
+  int filled_address = 0;
+  char temp_buff[NAME_LENGHT]; // temp buffer
 
-  while(fgets(buffAddresses, ROW_LENGHT, pFile) != NULL){
-    printf("%s", buffAddresses);
+  while((fgets(temp_buff, NAME_LENGHT, pFile) != NULL) && (filled_address < ADDRESSES_ROWS)){
+    
+    // replacing \n for \O + filling up the 
+    temp_buff[strcspn(temp_buff, "\n")] = '\0';
+    strcpy(addresses[filled_address].buffName, temp_buff);
+
+    printf("%s", addresses[filled_address].buffName);
+
+    filled_address++;
+  }
+
+  if((filled_address >= ADDRESSES_ROWS) && (fgets(temp_buff, NAME_LENGHT, pFile))){
+    printf("Too much addresses...:(");
   }
 
   return 0;
